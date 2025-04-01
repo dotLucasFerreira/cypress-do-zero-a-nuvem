d4f3b40c2453cb720f9b88e986e132140d4e64e3
Lucas Ferreira <l_ms_lucas@hotmail.com>
2025-04-01 20:08:53 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig} = require(`cypress`)
 
 module.exports = defineConfig({
+  projectId: "8r6ddg",
   viewportHeight: 660,
   viewportWidth: 1000,
   e2e: {},
   // video:true
 })
+
